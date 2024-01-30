ede920ed942952a003620d1626e1ed6df3f1cfcb
Doruk Cem <doruk.cem@gmail.com>
2024-01-30 12:45:53 +0300

    commits started

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File, Depends
+from fastapi import FastAPI, UploadFile, File, Depends, HTTPException, status
 from typing import List
 from .database import engine
 from sqlalchemy.orm import Session
@@ -13,21 +13,50 @@ models.Base.metadata.create_all(engine)
 def index():
    return {"Hello World"}
 
-@app.post("/upload")
+@app.post("/upload", status_code=status.HTTP_201_CREATED)
 def upload(files: List[UploadFile] = File(..., description= "Upload your files"), db: Session = Depends(database.get_db)):
+   """
+      Uploads all given files. Skips files that are already in the db. 
+   """
    try:
+      commit_message = "Hardcoded commit message"
+      commit_oid = "hardcode 123"
+      new_commit = models.Commit(commit_message=commit_message, oid = commit_oid, parent = None)
+
       for file in files:
          contents = file.file.read()
+         # ! Check if contents are null
          obj_oid = hashlib.sha1(contents).hexdigest()
+
+         existing_object = db.query(models.Object).filter_by(oid=obj_oid).first()
+         if existing_object:
+            continue
+
          new_object = models.Object(name=file.filename, blob=contents, oid= str(obj_oid) )
+         new_commit.objects.append(new_object)
          db.add(new_object)
+      
+      db.add(new_commit)
       db.commit()
 
    except Exception as e:
-      db.rollback
-      return {"message": f"There was an error uploading the file(s): {e}"}
+      db.rollback()
+      raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"{e}")
    finally:
       db.close()
 
-   return {"message": f"Successfuly uploaded {[file.filename for file in files]}"}    
+   return {"message": f"Successfuly uploaded {[file.filename for file in files]}"}
 
+@app.get("/read_file")
+def read_file(obj_oid : str, db: Session = Depends(database.get_db)):
+   """ Returns the contents of a file """
+   object = db.query(models.Object).filter_by(oid = obj_oid).first()
+   if not object:
+      raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Object not found")
+   
+   contents = object.blob.decode("utf-8")
+   
+   return {"content": contents}
+   # The returned content has special chars such as \n and \r
+
+# TODO some sort of error message on commits
--- app/models.py
+++ app/models.py
@@ -4,6 +4,21 @@ from sqlalchemy.orm import relationship
 
 class Object(Base):
    __tablename__ = "objects"
-   oid = Column(String, primary_key=True)
+   oid = Column(String, primary_key= True)
    name = Column(String)
    blob = Column(BLOB)
+
+   commits = relationship("Commit", secondary="commit_object_association")
+
+class Commit(Base):
+   __tablename__ = "commits"
+   oid = Column(String, primary_key= True)
+   commit_message = Column(String)
+   parent = Column(String, nullable= True)
+
+   objects = relationship("Object", secondary="commit_object_association")
+
+class CommitObjectAssociation(Base):
+   __tablename__ = 'commit_object_association'
+   commit_oid = Column(String, ForeignKey('commits.oid'), primary_key=True)
+   object_oid = Column(String, ForeignKey('objects.oid'), primary_key=True)
