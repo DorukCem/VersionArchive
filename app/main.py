92cccee98d2733d704fafe8b40dedb5b01c2f8b4
Doruk Cem <doruk.cem@gmail.com>
2024-02-16 16:41:40 +0300

    requirements

--- app/main.py
+++ app/main.py
@@ -6,9 +6,7 @@ from .routers import repository, objects, commits, branch, authentication, users
 # * We are currently ignoring folders and only taking in files
 
 # TODO users should only be allowed to work on things that they own
-# TODO schemas
 # TODO docs
-# TODO requirments
 
 # * Features will not be added until things above are done
 # TODO allow branching from previous commits
