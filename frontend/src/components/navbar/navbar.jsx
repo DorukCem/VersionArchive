875ece1065d47f6072d2b990bb0ea2736c881c0c
Doruk Cem <doruk.cem@gmail.com>
2024-04-10 18:26:23 +0300

    navbar with mobile

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,37 +1,45 @@
 import { NavLink } from "react-router-dom";
 import "./navbar-styles.css";
+import { useState } from "react";
 
 const navLinkStyle = {
-  textDecoration: 'none',
-  fontSize: '1.3rem',
+  textDecoration: "none",
+  fontSize: "1.3rem",
   fontWeight: 600,
-  color: '#fff'
+  color: "#fff",
 };
 
 export default function Navbar() {
+  const [menuClicked, setMenuClicked] = useState(false);
+
+  function handleClick() {
+    setMenuClicked(!menuClicked);
+  }
+
   return (
     <nav>
       <form>
         <input type="text" name="search" placeholder="Search for User" />
       </form>
-      <ul id="navbar">
+      <ul id="navbar" className={menuClicked ? "#navbar active" : "navbar"}>
         <li>
-          <NavLink
-            to={"/"}
-            style={navLinkStyle}
-          >
+          <NavLink to={"/"} style={navLinkStyle}>
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink
-            to={"/tutorial"}
-            style={navLinkStyle}
-          >
+          <NavLink to={"/tutorial"} style={navLinkStyle}>
             Tutorial
           </NavLink>
         </li>
       </ul>
+
+      <div id="mobile" onClick={handleClick}>
+        <i
+          id="bar"
+          className={menuClicked ? "fas fa-times" : "fas fa-bars"}
+        ></i>
+      </div>
     </nav>
   );
 }
