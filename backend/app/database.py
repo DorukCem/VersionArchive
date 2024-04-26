ffb80aa7be9a6f124b4684dbaf455d2ff3bb5871
Doruk Cem <doruk.cem@gmail.com>
2024-04-26 11:57:13 +0300

    updated python requirments

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from contextlib import contextmanager
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./app.db'
 
