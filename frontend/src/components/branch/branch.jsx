f34e869963a83dbdaad37607c97cd4d5a90ebc69
Doruk Cem <doruk.cem@gmail.com>
2024-04-29 10:59:41 +0300

    view previous commits button

--- frontend/src/components/branch/branch.jsx
+++ frontend/src/components/branch/branch.jsx
@@ -11,6 +11,7 @@ export default function Branch({ branchName, setRepoNotFound }) {
   const [branchData, setBranchData] = useState(null);
   const [commits, setCommits] = useState([]);
   const [selectedCommit, setSelectedCommit] = useState(null);
+  const [showCommits, setShowCommits] = useState(false);
 
   useEffect(() => {
     async function fetchBranchContents() {
@@ -62,14 +63,32 @@ export default function Branch({ branchName, setRepoNotFound }) {
     setRefresh(refresh + 1);
   };
 
+  const toggleShowCommits = () => {
+    setShowCommits(!showCommits);
+  };
+
   const handleCommitClick = (commit_oid) => {
     setSelectedCommit(commit_oid);
   };
 
   function formatDateTime(dateTimeString) {
     const dateTime = new Date(dateTimeString);
-    const options = { year: 'numeric', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' };
-    return dateTime.toLocaleDateString('en-US', options);
+    const options = {
+      year: "numeric",
+      month: "short",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    };
+    return dateTime.toLocaleDateString("en-US", options);
+  }
+
+  // Some things can only be done on head commit and others only on previous commits such as reset()
+  function is_head_commit() {
+    return (
+      selectedCommit === null || selectedCommit === branchData.head_commit_oid
+    );
   }
 
   return (
@@ -86,25 +105,41 @@ export default function Branch({ branchName, setRepoNotFound }) {
 
       {!buttonPressed && (
         <div>
+          {is_head_commit() && (
+            <div>
+              <Protected>
+                <button onClick={createNewCommit}>Commit files</button>
+              </Protected>
+            </div>
+          )}
+
           <div>
-            <Protected>
-              <button onClick={createNewCommit}>Commit files</button>
-            </Protected>
+            <button onClick={toggleShowCommits}>
+              {showCommits ? "Hide previous commits" : "View previous commits"}
+            </button>
           </div>
-          <div>
-            <h2>Commits in {branchName}:</h2>
-            <ul>
-              {commits.map((commit, index) => (
-                <li
-                  key={index}
-                  onClick={() => handleCommitClick(commit.oid)}
-                  style={{ cursor: "pointer" }}
-                >
-                  {`Message: ${commit.commit_message}  |  Time: ${formatDateTime(commit.timestamp)}`}
-                </li>
-              ))}
-            </ul>
-          </div>
+
+          {showCommits && (
+            <div>
+              <h2>Commits in {branchName}:</h2>
+              <ul>
+                {commits.map((commit, index) => (
+                  <li
+                    key={index}
+                    onClick={() => handleCommitClick(commit.oid)}
+                    className={
+                      commit.oid === selectedCommit ? "selected-commit" : ""
+                    }
+                    style={{ cursor: "pointer" }}
+                  >
+                    {`Message: ${
+                      commit.commit_message
+                    }  |  Time: ${formatDateTime(commit.timestamp)}`}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       )}
       {buttonPressed && (
